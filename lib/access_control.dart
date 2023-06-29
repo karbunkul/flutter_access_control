718d9199d3db999c437c8c9432e19e3eafa4195e
apohodun <apohodun@sdvor.com>
2023-06-29 09:58:08 +0500

    Export PermissionResolver

--- lib/access_control.dart
+++ lib/access_control.dart
@@ -5,3 +5,4 @@ export 'src/access_control_builder.dart';
 export 'src/control_mode.dart';
 export 'src/permission.dart';
 export 'src/permission_predicate.dart';
+export 'src/permission_resolver.dart';
